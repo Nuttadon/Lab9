a937705cc6b6ad48de5f674650377a2e95008556
Nuttadon <nuttadon_p@cmu.ac.th>
2021-12-06 20:50:05 +0700

    Lab9_2 complete

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -4,6 +4,20 @@
 using namespace std;
 
 //[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double x){
+    char y;
+   if(x>90)
+    y='A';
+   else if(x>75)
+    y='B';
+   else if(x>60)
+    y='C';
+   else if(x>45)
+    y='D';
+   else
+    y='F';
+   return y;
+}
 
 int main(){
 	//Input the number of students
@@ -18,8 +32,10 @@ int main(){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
 		//[Missing Code 3] Get score of the i-th students.
+		cin>>score[i];
 		i++;
 	}
 	
